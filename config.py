eda4b0454e98422897d5e11b73256125cad35ef9
limyongzhe <154297126+limyongzhe@users.noreply.github.com>
2023-12-19 11:44:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cmz2imknktsv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limyongzhe-employee"
+customregion = "us-east-1"
+
